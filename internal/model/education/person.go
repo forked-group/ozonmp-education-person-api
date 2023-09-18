f844cf46ced08d178ee7a0ba55bc8e48045f2372
Alex Pa <aaa2ppp@gmial.com>
2023-09-18 04:39:47 +0300

    Person.String() now returns a string in JSON

--- internal/model/education/person.go
+++ internal/model/education/person.go
@@ -1,10 +1,15 @@
 package education
 
+import (
+	"encoding/json"
+)
+
 type Person struct {
-	ID   uint64
-	Name string
+	ID   uint64 `json:"id"`
+	Name string `json:"name"`
 }
 
 func (p Person) String() string {
-	return p.Name
+	buf, _ := json.Marshal(p)
+	return string(buf)
 }
