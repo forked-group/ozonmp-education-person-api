44ee29fdfc2827637c82091011df60e6952a8f89
Aleksandr Panfilov <aleksandr@panfilov.pp.ru>
2023-09-28 17:21:15 +0300

    retranslator tests updated

--- internal/app/retranslator/retranslator_test.go
+++ internal/app/retranslator/retranslator_test.go
@@ -94,68 +94,183 @@ func TestStart(t *testing.T) {
 }
 
 func TestStart2(t *testing.T) {
-	repoCfg := DummyRepoCfg{
-		Size:       20_000,
-		Timeout:    100 * time.Millisecond,
-		ErrPer100K: 50_000,
+	tests := []struct {
+		name            string
+		repoCfg         DummyRepoCfg
+		senderCfg       DummySenderCfg
+		retranslatorCfg retranslator.Config
+	}{
+		{
+			"small",
+			DummyRepoCfg{
+				Size:       20,
+				Latency:    25 * time.Millisecond,
+				ErrPer100K: 30_000,
+			},
+			DummySenderCfg{
+				Size:       20,
+				Latency:    50 * time.Millisecond,
+				ErrPer100K: 30_000,
+			},
+			retranslator.Config{
+				ChannelSize:    0,
+				ConsumerCount:  3,
+				ConsumeSize:    4,
+				ConsumeTimeout: 50 * time.Millisecond,
+
+				ProducerCount:  10,
+				ProduceTimeout: 100 * time.Millisecond,
+
+				CollectSize:     4,
+				CollectMaxDelay: 50 * time.Millisecond,
+
+				WorkerCount: 2,
+				WorkTimeout: 100 * time.Millisecond,
+			},
+		},
+		{
+			"big",
+			DummyRepoCfg{
+				Size:       20_000,
+				Latency:    25 * time.Millisecond,
+				ErrPer100K: 30_000,
+			},
+			DummySenderCfg{
+				Size:       20_000,
+				Latency:    50 * time.Millisecond,
+				ErrPer100K: 30_000,
+			},
+			retranslator.Config{
+				ChannelSize:    0,
+				ConsumerCount:  2,
+				ConsumeSize:    100,
+				ConsumeTimeout: 100 * time.Millisecond,
+
+				ProducerCount:  400,
+				ProduceTimeout: 50 * time.Millisecond,
+
+				CollectSize:     100,
+				CollectMaxDelay: 50 * time.Millisecond,
+
+				WorkerCount: 4,
+				WorkTimeout: 100 * time.Millisecond,
+			},
+		},
 	}
 
-	senderCfg := DummySenderCfg{
-		Size:       repoCfg.Size,
-		Timeout:    50 * time.Millisecond,
-		ErrPer100K: 50_000,
-	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			repo := tt.repoCfg.New()
+			sender := tt.senderCfg.New()
 
-	repo := repoCfg.New()
-	sender := senderCfg.New()
+			tt.retranslatorCfg.Repo = repo
+			tt.retranslatorCfg.Sender = sender
 
-	cfg := retranslator.Config{
-		ChannelSize: 0,
+			ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+			defer cancel()
 
-		ConsumerCount:  2,
-		ConsumeSize:    100,
-		ConsumeTimeout: 100 * time.Millisecond,
+			tm := time.NewTimer(10 * time.Second)
+			tk := time.NewTicker(100 * time.Millisecond)
+			r := tt.retranslatorCfg.Start(ctx)
 
-		ProducerCount:  100,
-		ProduceTimeout: 100 * time.Millisecond,
+		loop:
+			for ctx.Err() == nil {
+				select {
+				case <-ctx.Done():
+					break loop
+				case <-tm.C:
+					break loop
+				case <-tk.C:
+					n := sender.Len()
+					if tt.repoCfg.Size <= 20 {
+						t.Logf("sent events: %d\r", n)
+					}
+					if n == tt.repoCfg.Size {
+						break loop
+					}
+				}
+			}
 
-		CollectSize:     100,
-		CollectMaxDelay: 100 * time.Millisecond,
+			tm.Stop()
+			tk.Stop()
+			r.Close()
 
-		WorkerCount: 8,
-		WorkTimeout: 100 * time.Millisecond,
+			deferred := repo.deferred
+			processed := repo.processed.Values()
+			removed := repo.removed
+			sent := sender.sent
 
-		Repo:   repo,
-		Sender: sender,
-	}
+			sortEvents := func(events []uint64) {
+				sort.Slice(events, func(i, j int) bool {
+					return events[i] < events[j]
+				})
+			}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
+			sortEvents(removed)
+			sortEvents(sent)
 
-	r := cfg.Start(ctx)
-	time.Sleep(5 * time.Second)
-	r.Close()
+			if tt.repoCfg.Size <= 20 {
+				sortEvents(deferred)
+				sortEvents(processed)
+				t.Logf("deferred : %v\n", deferred)
+				t.Logf("processed: %v\n", processed)
+				t.Logf("removed  : %v\n", removed)
+				t.Logf("sent     : %v\n", sent)
+			} else {
+				t.Logf("len(deferred) : %v\n", len(deferred))
+				t.Logf("len(processed): %v\n", len(processed))
+				t.Logf("len(removed)  : %v\n", len(removed))
+				t.Logf("len(sent)     : %v\n", len(sent))
+			}
 
-	sortEvents := func(events []uint64) {
-		sort.Slice(events, func(i, j int) bool {
-			return events[i] < events[j]
+			if len(processed) != 0 {
+				t.Error("")
+			}
+
+			if got := len(deferred) + len(removed) + len(processed); got != tt.repoCfg.Size {
+				t.Errorf("the number of repository events has changed. got %d, want %d", got, tt.repoCfg.Size)
+			}
+
+			checkEvents := func() bool {
+				events := make([]bool, tt.repoCfg.Size+1)
+
+				for _, ev := range deferred {
+					if events[ev] {
+						return false
+					}
+				}
+
+				for _, ev := range processed {
+					if events[ev] {
+						return false
+					}
+				}
+
+				for _, ev := range removed {
+					if events[ev] {
+						return false
+					}
+				}
+
+				return true
+			}
+
+			if !checkEvents() {
+				t.Error("repository event IDs are not consistent")
+			}
+
+			if got := repo.missed; got != 0 {
+				t.Errorf("missed repository updates = %d, want 0", got)
+			}
+
+			if got := len(processed); got != 0 {
+				t.Errorf("there are %d sent left in processing, want 0", got)
+			}
+
+			if !reflect.DeepEqual(removed, sent) {
+				t.Error("IDs of the removed and sent events do not match")
+			}
 		})
 	}
 
-	sortEvents(repo.Removed)
-	sortEvents(sender.Events)
-
-	if repoCfg.Size <= 20 {
-		t.Logf("repo.Deferred: %v\n", repo.Deferred)
-		t.Logf("repo.Removed : %v\n", repo.Removed)
-		t.Logf("sender.Events: %v\n", sender.Events)
-	} else {
-		t.Logf("len(repo.Deferred): %v\n", len(repo.Deferred))
-		t.Logf("len(repo.Removed) : %v\n", len(repo.Removed))
-		t.Logf("len(sender.Events): %v\n", len(sender.Events))
-	}
-
-	if !reflect.DeepEqual(repo.Removed, sender.Events) {
-		t.Error("lists of removed events and sent events are not equal")
-	}
 }
